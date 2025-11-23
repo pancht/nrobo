430ded2fb1da48098a05d1eca37a4589e0081841
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2025-11-24 03:51:06 +0530

    fix accepting pytest args

--- src/nrobo/cli.py
+++ src/nrobo/cli.py
@@ -1,22 +1,29 @@
-import typer
 import pytest
+import typer
+
 from .runner import run_tests
 
-app = typer.Typer(help="nRoBo - Smart Test Runner built on Pytest")
+app = typer.Typer(help="nRoBo - Smart Test Runner built on Pytest",
+                  add_completion=False,
+                  context_settings={"allow_extra_args": True, "ignore_unknown_options": True},
+                  )
+
 
 @app.command()
-def run(
-    suite: str = typer.Option(None, "--suite", "-s", help="Suite YAML file name under suites/"),
-    verbose: bool = typer.Option(False, "--verbose", "-v", help="Enable verbose output"),
-    **pytest_args
+def run(ctx: typer.Context,
+        suite: str = typer.Option(None, "--suite", "-s", help="Suite YAML file name under suites/"),
 ):
     """Run tests via nrobo."""
+    pytest_args = ctx.args
     typer.echo("Starting nRoBo test execution...")
-    exit_code = run_tests(suite=suite, verbose=verbose, pytest_args=pytest_args)
+    pytest_options = run_tests(suite=suite, pytest_args=pytest_args)
+    exit_code = pytest.main(pytest_options)
     raise typer.Exit(code=exit_code)
+
 
 def main():
     app()
+
 
 if __name__ == "__main__":
     main()
--- src/nrobo/runner.py
+++ src/nrobo/runner.py
@@ -2,7 +2,7 @@ import pytest
 import yaml
 from pathlib import Path
 
-def run_tests(suite=None, verbose=False, pytest_args=None):
+def run_tests(suite=None, pytest_args=None):
     test_dir = Path.cwd() / "tests"
     suite_dir = Path.cwd() / "suites"
 
@@ -16,11 +16,9 @@ def run_tests(suite=None, verbose=False, pytest_args=None):
                 selected_tests.append(str(test_dir / t))
 
     pytest_options = selected_tests
-    if verbose:
-        pytest_options.append("-v")
 
     if pytest_args:
         for k, v in pytest_args.items():
             pytest_options.append(f"--{k}={v}")
 
-    return pytest.main(pytest_options)
+    return pytest_options
