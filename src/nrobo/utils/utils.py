8322844ac871090cd2e184f118cbbd97d21b7cc3
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2025-11-25 16:58:36 +0530

    added more dirs

--- src/nrobo/utils/utils.py
+++ src/nrobo/utils/utils.py
@@ -12,12 +12,18 @@ def initialize_project():
     ui_dir = tests_dir / "ui"
     api_dir = tests_dir / "api"
     mobile_dir = tests_dir / "mobile"
+    reports_dir = base_dir / "reports"
+    allure_reports_dir = base_dir / "allure-reports"
+    allure_results_dir = base_dir / "allure-results"
 
     suites_dir.mkdir(exist_ok=True)
     tests_dir.mkdir(exist_ok=True)
     ui_dir.mkdir(exist_ok=True)
     api_dir.mkdir(exist_ok=True)
     mobile_dir.mkdir(exist_ok=True)
+    reports_dir.mkdir(exist_ok=True)
+    allure_reports_dir.mkdir(exist_ok=True)
+    allure_results_dir.mkdir(exist_ok=True)
 
     # Copy template files from package templates
     with res.files("nrobo.templates").joinpath("sample_suite.yml").open("rb") as src:
