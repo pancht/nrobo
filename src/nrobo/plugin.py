f029e2ed75981b97a4f24d5da75e66bc8cded955
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2025-11-25 08:45:07 +0530

    fixed linter errors

--- src/nrobo/plugin.py
+++ src/nrobo/plugin.py
@@ -1,9 +1,11 @@
 import logging
+import os
 
 import pytest
 from selenium.webdriver.remote.webdriver import WebDriver
 from .drivers.driver_factory import get_driver
 from nrobo.selenium_wrappers.nrobo_selenium_wrapper import NRoboSeleniumWrapperClass
+from _pytest.fixtures import FixtureRequest
 
 
 class nRoboWebDriverPlugin:
@@ -25,16 +27,42 @@ class nRoboWebDriverPlugin:
         )
         parser.addoption("--auto-driver", action="store_true", help="Auto use driver in tests")
 
+    def _get_logger(self, request: FixtureRequest) -> logging.Logger:
+        test_name = request.node.name
+        log_dir = os.path.join("logs")
+        os.makedirs(log_dir, exist_ok=True)
+
+        logger = logging.getLogger(f"nrobo.{test_name}")
+        logger.setLevel(logging.DEBUG)
+
+        if not logger.handlers:  # Avoid duplicate handlers
+            # Console Handler
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.DEBUG)
+            ch.setFormatter(logging.Formatter("[%(levelname)s] %(message)s"))
+
+            # File Handler
+            fh = logging.FileHandler(os.path.join(log_dir, f"{test_name}.log"))
+            fh.setLevel(logging.DEBUG)
+            fh.setFormatter(logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            ))
+
+            logger.addHandler(ch)
+            logger.addHandler(fh)
+
+        return logger
+
     @pytest.fixture(scope="function", autouse=False)
     def driver(self, request):
         browser = request.config.getoption("--browser")
         headless = not request.config.getoption("--no-headless")
-        self.driver_instance:WebDriver = get_driver(browser, headless=headless)
+        self.driver_instance: WebDriver = get_driver(browser, headless=headless)
 
-        # Create a logger
-        logger = logging.getLogger(f"test_logger.{request.node.name}")
-        logger.setLevel(logging.DEBUG)
+        logger = self._get_logger(request)
+        # Inject logger
+        wrapper: NRoboSeleniumWrapperClass = NRoboSeleniumWrapperClass(self.driver_instance, logger=logger)
 
-        wrapper:NRoboSeleniumWrapperClass = NRoboSeleniumWrapperClass(self.driver_instance,logger=logger)
         yield wrapper
+
         self.driver_instance.quit()
