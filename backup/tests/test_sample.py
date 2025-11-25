c356f4e4c77d499507254f5c0270aec1cb13062b
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2025-11-25 23:45:19 +0530

    removed backup folder

--- backup/tests/test_sample.py
+++
@@ -1,8 +0,0 @@
-from nrobo.selenium_wrappers.nrobo_selenium_wrapper import NRoboSeleniumWrapperClass
-
-
-def test_a(driver:NRoboSeleniumWrapperClass):
-    driver.logger.info("HI")
-
-def test_b(driver:NRoboSeleniumWrapperClass):
-    driver.logger.info("BYE")
