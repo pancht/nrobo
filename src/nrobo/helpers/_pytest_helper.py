40424b184eab85d9d6873ebef7b295e5304dfe7b
Panchdev Singh Chauhan <1681515+pancht@users.noreply.github.com>
2025-12-01 10:05:53 +0530

    Update _pytest_helper.py
    Signed-off-by: Panchdev Singh Chauhan <1681515+pancht@users.noreply.github.com>

--- src/nrobo/helpers/_pytest_helper.py
+++ src/nrobo/helpers/_pytest_helper.py
@@ -72,10 +72,10 @@ def detect_fixture_usage(fixture_name: str, test_paths: List[str], pytest_args: 
             [
                 "pytest",
                 "--collect-only",
-                # "-p",
-                # "nrobo.plugins.detect_fixtures_plugin",
-                # *['--html=test_artifacts/html_report/report.html', '--self-contained-html', '--alluredir', 'test_artifacts/allure-results'],
-                # *test_paths,
+                "-p",
+                "nrobo.plugins.detect_fixtures_plugin",
+                *pytest_args,
+                *test_paths,
             ]
         )
 
