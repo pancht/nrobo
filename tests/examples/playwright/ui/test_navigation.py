4c91edb35f574919fa432625d159fd6dd59c11e6
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2025-12-17 03:36:20 +0530

    add a playwright test

--- tests/examples/playwright/ui/test_navigation.py
+++ tests/examples/playwright/ui/test_navigation.py
@@ -1,5 +1,17 @@
-from playwright.async_api import Page
+from playwright.sync_api import Page, expect
 
 
-def test_open_pypi(page: Page):
-    page.goto("https://google.com")
+def test_navigation_actions(page: Page):
+    page.goto("https://the-internet.herokuapp.com")
+
+    page.get_by_role("link", name="A/B Testing").click()
+
+    expect(page.get_by_role("heading"), "A/B Test Variation 1").to_be_visible()
+
+    page.go_back()
+
+    expect(page.get_by_role("heading"), "Welcome to the-internet")
+
+    page.go_forward()
+
+    expect(page.get_by_role("heading"), "A/B Test Variation 1").to_be_visible()
